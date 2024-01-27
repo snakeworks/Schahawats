9c0d41433d9be2e03ae4ca6c0e214b9403503466
Filip <filipsmolcec@gmail.com>
2024-01-27 15:41:36 +0100

    Updated .gitignore

---
+++ ChessUI/Database/ChessGame.cs
@@ -0,0 +1,13 @@
+﻿namespace ChessUI
+{
+    public class ChessGame
+    {
+        public int Id { get; set; }
+        public string SiteTitle { get; set; }
+        public string EventTitle { get; set; }
+        public string WhiteName { get; set; }
+        public string BlackName { get; set; }
+        public string Result { get; set; }
+        public string FullPgn { get; set; }
+    }
+}
